2cc19441338dbe159d5ede5b0fd73c01edfba6d4
H0R4T1U <horatiubanciu11@gmail.com>
2021-10-22 17:41:03 +0300

    added a queue and can now jump to the next song

--- src/commands/dami.js
+++ src/commands/dami.js
@@ -10,38 +10,57 @@ module.exports = {
 		.setName('dami')
 		.setDescription('Iti dau!')
 		.addStringOption(option => option.setName("pesa").setDescription("Numele pesei").setRequired(true)),
+	playlist:[],
+	player: new createAudioPlayer(),
+	i:0,//static foloseste
 	async execute(interaction) {
 		const piesa = interaction.options.getString("pesa");
 		const connection = getVoiceConnection(interaction.guildId);
-		const player = createAudioPlayer();
-		let link;
-		if(!(piesa.startsWith('https://'))){
-			await youtube.search(piesa).then(results=>{
-				link = results[0].url;
+		let stream;
+		await youtube.search(piesa).then(results=>{
+			if(results){
+				this.playlist.push(results[0]);
+			}else{
+				interaction.reply("Couldn't find shit!")
+				return;
+			}
+			
+		})
+		if(!(this.player.state.status == "playing" || this.player.state.status == "buffering")){
+			stream = await ytdl(this.playlist[this.i++].url, {
+				filter: "audioonly",
+				opusEncoded: false,
+				fmt: "mp3",
+				encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+			});
+
+			const resource = createAudioResource(stream,{ inlineVolume:true });
+			resource.volume.setVolume(0.5);
+
+			this.player.play(resource);
+			connection.subscribe(this.player);
+			
+			this.player.on(AudioPlayerStatus.Idle,async ()=>{
+				stream = await ytdl(this.playlist[this.i++].url, {
+					filter: "audioonly",
+					opusEncoded: false,
+					fmt: "mp3",
+					encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+				});
+	
+				const resource = createAudioResource(stream,{ inlineVolume:true });
+				resource.volume.setVolume(0.5);
+	
+				this.player.play(resource);
+				
 			})
 			
+			await interaction.reply("Stai ca o indoi amu coae");
+
 		}else{
-			link = piesa;
-
+			
+			await interaction.reply("Am adaugat pesa la playlist frate!");
+			
 		}
-
-		console.log(`Se canta pesa:${link}`);
-		let stream = await ytdl(link, {
-			filter: "audioonly",
-			opusEncoded: false,
-			fmt: "mp3",
-			encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
-		});
-
-		const resource = createAudioResource(stream,{ inlineVolume:true });
-		resource.volume.setVolume(0.5);
-
-		player.play(resource);
-		connection.subscribe(player);
-		
-		
-		await interaction.reply("Stai ca o indoi amu coae");
-
-		
 	},
 };
